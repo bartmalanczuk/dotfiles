8871388521959b51a92c529e0c33a329a1f6fa8d
bartmalanczuk <bart.malanczuk@gmail.com>
2024-10-20 00:30:52 +0200

    add tsx to treesitter

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,9 +1,9 @@
 return {
-    'nvim-treesitter/nvim-treesitter', 
+    'nvim-treesitter/nvim-treesitter',
     build = ":TSUpdate",
     config = function()
         require('nvim-treesitter.configs').setup({
-            ensure_installed = { 'javascript', 'typescript', 'c', 'lua', 'vim', 'vimdoc', 'query' },
+            ensure_installed = { 'javascript', 'typescript', 'tsx', 'c', 'lua', 'vim', 'vimdoc', 'query' },
             sync_install = false,
             highlight = {
               enable = true,
